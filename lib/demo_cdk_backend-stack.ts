5aa25f104224a8be49e645daf71195a1523c2ab8
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-11-28 23:31:58 +0500

    Initialized and Defined DynamoDB
    - Initialized CDK App and Defined DynamoDB Table

---
+++ lib/demo_cdk_backend-stack.ts
@@ -0,0 +1,17 @@
+import * as cdk from '@aws-cdk/core';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+
+export class DemoCdkBackendStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Created a DynamoDB Table
+    const myDynamoDbProductTable = new dynamodb.Table(this, 'myDynamoDBTable', {
+      tableName: 'My_DynamoDB_TABLE_FOR_CICD',
+      partitionKey: {
+        name: 'id',
+        type: dynamodb.AttributeType.STRING
+      }
+    })
+  }
+}
